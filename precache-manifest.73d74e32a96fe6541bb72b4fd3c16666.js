4321db967244047191674ed4fbea091898e59598
ce <soooqa02@gmail.com>
2019-01-22 20:31:13 +0600

    Updates

---
+++ precache-manifest.73d74e32a96fe6541bb72b4fd3c16666.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/vkapps-flappy/static/media/persik.4e1ec840.png"
+  },
+  {
+    "revision": "1289425f86748319b537",
+    "url": "/vkapps-flappy/static/js/runtime~main.1289425f.js"
+  },
+  {
+    "revision": "ff439f0ee4e3ac7a88e2",
+    "url": "/vkapps-flappy/static/js/main.ff439f0e.chunk.js"
+  },
+  {
+    "revision": "8df0363b7eab98c34d53",
+    "url": "/vkapps-flappy/static/js/1.8df0363b.chunk.js"
+  },
+  {
+    "revision": "ff439f0ee4e3ac7a88e2",
+    "url": "/vkapps-flappy/static/css/main.9de27310.chunk.css"
+  },
+  {
+    "revision": "8df0363b7eab98c34d53",
+    "url": "/vkapps-flappy/static/css/1.adc77026.chunk.css"
+  },
+  {
+    "revision": "fcb444e18c09b82f59282a6463aa9f2a",
+    "url": "/vkapps-flappy/index.html"
+  }
+];
